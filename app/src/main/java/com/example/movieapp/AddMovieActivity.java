19b5ca421e0c43d453ea7fd9ea02694126f68ae8
meghnabiju <meghnaparayil@gmail.com>
2024-12-17 15:29:05 +0530

    viewpage updated

--- app/src/main/java/com/example/movieapp/AddMovieActivity.java
+++ app/src/main/java/com/example/movieapp/AddMovieActivity.java
@@ -11,36 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuActivity extends AppCompatActivity {
+public class AddMovieActivity extends AppCompatActivity {
 
-    Button b1,b2,b3,b4;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_add_movie);
 
-        b1=(Button) findViewById(R.id.a);
-        b2=(Button) findViewById(R.id.b);
-        b3=(Button) findViewById(R.id.c);
-        b4=(Button) findViewById(R.id.d);
-
+        b1=(Button) findViewById(R.id.back);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent ob=new Intent(getApplicationContext(), AddMovieActivity.class);
+                Intent ob=new Intent(getApplicationContext(), MenuActivity.class);
                 startActivity(ob);
             }
         });
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent obj=new Intent(getApplicationContext(), ViewMovieActivity.class);
-                startActivity(obj);
-            }
-        });
-
-
     }
 }
